76ebef1673fe0d8dee738f7eef5a6359b3f3b7b4
jaykishan-2450 <122507794+jaykishan-2450@users.noreply.github.com>
2024-02-14 13:06:42 +0530

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact=()=>{
+    return(
+        <div style={{margin:"20px 20px 20px 200px"}}>
+            <h2 style={{textDecoration:"underline"}}>Developers:</h2>
+            <ol>
+                <li>Jay Kishan</li>
+                <li>Drishti jain</li>
+                <li>Keshav agarwal</li>
+                <li>kavya Choudhary</li>
+            </ol>
+        </div>
+
+    )
+}
+export default Contact;
